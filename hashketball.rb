b31414497d4f3151dcfcf55a8e39d0b6ae2b940e
Elias Campos <ecampose76@gmail.com>
2020-11-19 01:37:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -174,7 +174,7 @@ end
 def player_numbers(input)
   output = []
   game_hash.each do |home_or_visitor, team_info|
-    if team_info[:team_name] = input
+    if team_info[:team_name] == input
       team_info.each do |attribute, value|
         if attribute == :players
           value.each do |player|
@@ -186,20 +186,3 @@ def player_numbers(input)
   end
   return output
 end
-
-def player_numbers(input)
-  output = []
-  game_hash.each do |team, team_info|
-    if team_info[:team_name] == input 
-      team_info.each do |key, value|
-        if key == :players
-          value.each do |player|
-          output.push(player[:number])
-          end
-        end
-      end
-    end
-  end
-  return output
-end
-
