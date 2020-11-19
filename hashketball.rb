77abdebce277eb7a7ece653765fcf8a4d17bf317
Elias Campos <ecampose76@gmail.com>
2020-11-19 01:54:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -193,7 +193,6 @@ def player_stats(input)
       if key == :players
         value.each do |player|
           if input == player[:player_name]
-            player(:player_name)
             return player
           end
         end
